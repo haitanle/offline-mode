05f7cdedfb3ad573e7f1e008e2ee321ace8fad22
tan <haitanle88@gmail.com>
2019-02-28 13:08:47 -0500

    add cache response

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,4 +1,5 @@
 self.addEventListener('install', function(event) {
+
   event.waitUntil(
     caches.open('wittr-static-v1').then(function(cache) {
       return cache.addAll([
@@ -16,4 +17,10 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   // TODO: respond with an entry from the cache if there is one.
   // If there isn't, fetch from the network.
+  event.respondWith(
+	caches.match(event.request).then(function(response){
+	if (response) return response;
+	return fetch(event.request);
+	})
+  );
 });
